e72a23ef3a33dddf68b267ea3f8724952f05e2a1
Guite <info@guite.de>
2020-04-06 18:32:58 +0200

    typo

--- entrypoint.sh
+++ entrypoint.sh
@@ -11,7 +11,7 @@ CREATE_ARTIFACTS=${6:-false}
 DB_HOST=${7:-mysql}
 DB_PORT=${8:-default}
 if [ "$DB_PORT" = "default" ]; then
-    DB_HOST='3306'
+    DB_PORT='3306'
 fi
 DB_USER=${9:-zikula}
 DB_PASS=${10:-zikula}
@@ -22,18 +22,18 @@ if [ "$TOOLS" = "default" ]; then
     TOOLS=',phplint,parallel-lint,lint:container,lint:yaml,lint:twig,translations,doctrine-info,phpcs,php-cs-fixer,phpunit-bridge,security-checker,churn,phploc,phpmetrics,php-coupling-detector,deprecation-detector,phpinsights,'
 fi
 
-echo "Vendor: ${VENDOR_NAME}"
-echo "Module: ${MODULE_NAME}"
-echo "Version: ${APP_VERSION}"
-echo "Core: ${CORE}"
-echo "Base dir: ${BASE_DIR}"
-echo "Create artifacts: ${CREATE_ARTIFACTS}"
-echo "DB Host: ${DB_HOST}"
-echo "DB Port: ${DB_PORT}"
-echo "DB User: ${DB_USER}"
-echo "DB Pass: ${DB_PASS}"
-echo "DB Name: ${DB_NAME}"
-echo "Tools: ${TOOLS}"
+# echo "Vendor: ${VENDOR_NAME}"
+# echo "Module: ${MODULE_NAME}"
+# echo "Version: ${APP_VERSION}"
+# echo "Core: ${CORE}"
+# echo "Base dir: ${BASE_DIR}"
+# echo "Create artifacts: ${CREATE_ARTIFACTS}"
+# echo "DB Host: ${DB_HOST}"
+# echo "DB Port: ${DB_PORT}"
+# echo "DB User: ${DB_USER}"
+# echo "DB Pass: ${DB_PASS}"
+# echo "DB Name: ${DB_NAME}"
+# echo "Tools: ${TOOLS}"
 
 mysqlCmd="mysql -h ${DB_HOST} --port ${DB_PORT} -u ${DB_USER} -p${DB_PASS} -e"
 
